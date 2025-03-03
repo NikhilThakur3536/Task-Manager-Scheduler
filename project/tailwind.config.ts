c472320bfa5980c74d07150c9bb4b6059c57e271
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-03-03 18:15:23 +0530

    Tailwind v3 config update

--- project/tailwind.config.ts
+++ project/tailwind.config.ts
@@ -1,23 +1,15 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily:{
-        poppins:['var(--font-poppins)', 'sans-serif'],
-        montserrat:['var(--font-montserrat)','sans-serif'],
-        mulish:['var(--font-muli)','sans-serif']
-      }
-    },
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+}
